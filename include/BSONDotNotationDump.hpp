754a5a54cb0935d649d7d87076561e00b2f7ba68
Mark Deazley <mdeazley@gmail.com>
2013-12-21 10:28:34 -0700

    New class for dot notation dumps.

---
+++ include/BSONDotNotationDump.hpp
@@ -0,0 +1,126 @@
+/*!
+ * \file BSONObjectTypeDump.hpp
+ * \brief BSON Object Type Stream Definitions
+ *
+ * \author Mark Deazley &lt;mdeazley@gmail.com&gt;
+ * \copyright Copyright &copy; 2013 by Mark Deazley<br/><br/>
+ *
+ *   <b>License:</b> <i>Free Software Foundation’s GNU AGPL v3.0.</i><br/>
+ *
+ * This program is free software: you can redistribute it and/or modify<br/>
+ * it under the terms of the GNU Affero General Public License as<br/>
+ * published by the Free Software Foundation, either version 3 of the<br/>
+ * License, or (at your option) any later version.<br/>
+ *
+ * This program is distributed in the hope that it will be useful,<br/>
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of<br/>
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br/>
+ * GNU Affero General Public License for more details.<br/>
+ *
+ * You should have received a copy of the GNU Affero General Public License<br/>
+ * along with this program.  If not, see http://www.gnu.org/licenses/ .<br/>
+ * 
+ * Creation Date: Oct 30, 2013
+ * Eclipse Project: MongoType
+ * 
+ */
+
+//----------------------------------------------------------------------------
+
+#ifndef BSONDOTNOTATIONDUMP_HPP_
+#define BSONDOTNOTATIONDUMP_HPP_
+
+#include <mongotype.hpp>
+#include <BSONTypeMap.hpp>
+#include <BSONObjectParser.hpp>
+
+namespace mongotype {
+
+/*!
+ * \brief Human readable BSON Object Dump
+ *
+ * Provides an std::ostream comparable output operator for dumping a human readable text
+ * version of the given BSON object. BSONObjectTypeDump implements interface IBSONObjectVisitor and
+ * uses the BSON object parsing events to output the BSON object's text representation.
+ *
+ * \see IBSONObjectVisitor, BSONObjectParser
+ */
+
+class BSONDotNotationDump : virtual protected IBSONObjectVisitor {
+	const BSONObj& object;
+	deque<string> dotStack;
+	ostream* osp;
+
+protected: // IBSONObjectVisitor overrides.
+
+	virtual void onParseStart() { }
+
+	virtual void onParseEnd() {	}
+
+	virtual void onObjectStart(const BSONObj& object, int arrayIndex) {
+		if (arrayIndex >= 0) { // If the object is an element of an array...
+			 string s("[");
+			 s += to_string(arrayIndex);
+			 s += "]"; // Output an array index first.
+			 dotStack.push_back(s);
+		}
+	}
+
+	virtual void onObjectEnd(const BSONObj& object, int arrayIndex) {
+		if (arrayIndex >= 0) { // If the object is an element of an array...
+			dotStack.pop_back();
+		}
+	}
+
+	virtual void onArrayStart(const BSONElement& element, int count) {
+		string s(".");
+		s += element.fieldName();
+		 dotStack.push_back(s);
+	}
+
+	virtual void onArrayEnd(const BSONElement& element) {
+		dotStack.pop_back();
+	}
+
+	virtual void onElement(const BSONElement& element, int arrayIndex) {
+		BSONTypeMap type(element);
+		string acc;
+		for (string s: dotStack) {
+			acc += s;
+		}
+		acc += '.';
+		acc += string(element);
+		acc += " ";
+		acc += type.to_string();
+		*osp << acc << "\n";
+	}
+
+public: // User Interface
+
+	/*!
+	 * \brief Construct a BSON object dumper.
+	 * \param[in] pobject The BSON object to be dumped.
+	 * \param[in] pindentStr The string used to indent the text output. The indent text is prepended to the output lines once for each indent level.
+	 */
+	BSONDotNotationDump(const BSONObj& pobject, string& initialToken) : object(pobject), osp(NULL) {
+		dotStack.clear();
+		dotStack.push_back(initialToken);
+	};
+	virtual ~BSONDotNotationDump() {};
+
+	/*!
+	 * \brief BSON Object stream output operator.
+	 * \param[in,out] out The std::ostream to write the BSON object dump.
+	 * \param[in] bos The BSONObjectTypeDump object to write to the output stream.
+	 */
+
+	OSTREAM_FRIEND(BSONDotNotationDump& bos) {
+		bos.osp = &out; // Streams... Sigh.
+		BSONObjectParser objectParser(bos); // Construct a parser around this event handler.
+		objectParser.parse(bos.object);     // Parse the object and write the text output the the output stream.
+		return out;							// Required for stream chaining.
+	}
+};
+
+} /* namespace mongotype */
+#endif /* BSONDOTNOTATIONDUMP_HPP_ */
